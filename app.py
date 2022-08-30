77cbe96e0d1f867151d12d05ccacb6c17f07db84
Xintao <wxt1994@126.com>
2022-08-30 22:19:01 +0800

    update

--- app.py
+++ app.py
@@ -80,13 +80,9 @@ def inference(img, version, scale):
 
 
 title = "GFPGAN: Practical Face Restoration Algorithm"
-description = r"""
-Gradio demo for <a href='https://github.com/TencentARC/GFPGAN' target='_blank'><b>GFPGAN: Towards Real-World Blind Face Restoration with Generative Facial Prior</b></a>. <br>
-[![GitHub Stars](https://img.shields.io/github/stars/TencentARC/GFPGAN?style=social)](https://github.com/TencentARC/GFPGAN)
-It can be used to: <br>
-- Upsample/Restore your **old photos**
-- Upsample/Improve **AI-generated faces**
-
+description = r"""[![GitHub Stars](https://img.shields.io/github/stars/TencentARC/GFPGAN?style=social)](https://github.com/TencentARC/GFPGAN)
+Gradio demo for <a href='https://github.com/TencentARC/GFPGAN' target='_blank'><b>GFPGAN: Towards Real-World Blind Face Restoration with Generative Facial Prior</b></a>.<br>
+It can be used to restore your **old photos** or improve **AI-generated faces**.<br>
 To use it, simply upload your image. Please click submit only once.
 """
 article = r"""<p style='text-align: center'><a href='https://arxiv.org/abs/2101.04061' target='_blank'>GFPGAN: Towards Real-World Blind Face Restoration with Generative Facial Prior</a> | <a href='https://github.com/TencentARC/GFPGAN' target='_blank'>Github Repo</a></p><center><img src='https://visitor-badge.glitch.me/badge?page_id=akhaliq_GFPGAN' alt='visitor badge'></center>
